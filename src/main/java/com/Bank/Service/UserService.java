9c6c4baa8ea5f96ce23a59dda9039e70bf7822df
“ahmadbenali” <a7madbenali@gmail.com>
2026-06-24 20:58:21 +0300

    Add UserService for register

---
+++ src/main/java/com/Bank/Service/UserService.java
@@ -0,0 +1,49 @@
+package com.Bank.Service;
+
+
+import com.Bank.Model.User;
+import com.Bank.Repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository,  PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public User Register(User user) {
+        if(userRepository.existsByUsername(user.getUsername())) {
+            throw new RuntimeException("Username is already in use");
+        }
+
+        if(userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email is already in use");
+        }
+
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User profile not found with ID: " + id));
+    }
+
+
+}
